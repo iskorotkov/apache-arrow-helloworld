dc473fcf1b98c8c63381d68cc3cfb24302b739dd
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-08-22 22:10:44 +0500

    Add project files.

---
+++ src/ApacheArrowHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApacheArrowCs
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
